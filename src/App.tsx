5f27d0074a33140e9e5e568e8ba63408ac22efba
Alex <alexdfrontend@gmail.com>
2025-08-26 15:40:07 +0300

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Dates from './components/Dates/Dates';
+
+function App() {
+  return (
+    <div className="App">
+      <Dates />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dates/Dates.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Dates: React.FC<Props> = () => {
+    return <div>Hey</div>;
+};
+
+export default Dates;
